e09aebf9a18cffd913a3423ac2031f2e8556b3c0
Purab Singla <131240287+Purabsingla@users.noreply.github.com>
2025-03-14 21:48:42 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ App.post("/claim", async (req: Request, res: Response): Promise<any> => {
 
   if (lastClaim) {
         const lastClaimTime: Date = lastClaim.claimedAt.toDate(); // Convert Firestore Timestamp to Date
-        const cooldownEndTime: number = new Date(lastClaimTime.getTime() + 60 * 60 * 1000);
+        const cooldownEndTime: Date = new Date(lastClaimTime.getTime() + 60 * 60 * 1000);
         const timeLeft: number = Math.ceil((cooldownEndTime - Date.now()) / 60000); // Convert ms to minutes
 
         return res.status(403).json({
